90f024dac9608daec1cb3e48cd55f1d773a2cfcd
Alexandre <alexandre.spindola11@gmail.com>
2023-06-11 13:06:01 +0200

    syntax

---
+++ OpenBootcamp/sesion-02-syntax/syntax.js
@@ -0,0 +1,12 @@
+let lista = [
+    "Alexandre Spindola",
+    34, 
+    true,
+    "19-11-1988",
+    { 
+        titulo: "Florescer",
+        autor: "Martin Seligman",
+        fecha: 2011,
+        url: "https://www.amazon.com.br/Florescer-Martin-P-Seligman/dp/8539002868"
+    }
+]
